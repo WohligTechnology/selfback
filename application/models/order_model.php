f992cce5c632d800075a4b33cdb0d6b5dc3b0468
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-02-16 15:34:33 +0530

    place order emailer

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -106,15 +106,13 @@ class order_model extends CI_Model
          $order = $this->db->insert_id();
          $mysession['orderid'] = $order;
          $this->session->set_userdata($mysession);
-         $email =  $mysession['email'];
-              //  echo "email".$email;
 
 // echo"cart";
-print_r($mysession);
-     print_r($carts);
+// print_r($mysession);
+  print_r($carts);
    //        $cartcount=count($carts);
    //        echo "    cart count    ".$cartcount."      "."end";
-         foreach ($carts as $cart) {
+              foreach ($carts as $cart) {
              $querycart = $this->db->query("INSERT INTO `fynx_orderitem`(`order`, `product`, `quantity`,`status`, `price`, `finalprice`,`design`) VALUES ('$order','".$cart['id']."','".$cart['qty']."','".$cart['status']."','".$cart['price']."','".$cart['subtotal']."','".$cart['design']."')");
 
             // print_r($querycart);
@@ -131,7 +129,7 @@ print_r($mysession);
                 $this->email->from('vigwohlig@gmail.com', 'Selfcare');
                 $this->email->to($email);
                 $this->email->subject('Thank You for shopping with us');
-
+$message= "test text";
                 $message = "<html><body><div id=':1fn' class='a3s adM' style='overflow: hidden;'><div class='HOEnZb'><div class='adm'><div id='q_152da6db6beee01c_0' class='ajR h4' data-tooltip='Hide expanded content' aria-label='Hide expanded content'><div class='ajT'></div></div></div><div class='im'><u></u>
                 <div style='margin:0'>
 
@@ -144,9 +142,31 @@ print_r($mysession);
 
                 <p style='color:#000;font-family:Roboto;font-size:20px'>Thank You for shopping with SelfCare. You've picked up some really healthy and amazing stuff and we can't wait to have it delivered at your doorstep. Here's what you ordered:
    </p>
-   <p style='color:#000;font-family:Roboto;font-size:20px'>
+   <p style='color:#000;font-family:Roboto;font-size:20px'>";
 
-   cart Details
+   $count=1;
+   $finalpricetotal=0;
+   foreach($carts as $cart)
+   {
+       $id=$cart['id'];
+       $quantity=$cart['qty'];
+       $price=$cart['price'];
+
+       $message.="
+       <table>
+       <tr>
+           <td align='center' style='padding: 10px 0;'>$count</td>
+           <td align='center' style='padding: 10px 0;'>$id</td>
+           <td align='center' style='padding: 10px 0;'>$quantity</td>
+           <td align='center' style='padding: 10px 0;'>$price</td>
+        
+         </tr>
+         </table>";
+       $finalpricetotal=$finalpricetotal+$value->finalprice;
+                       $counter++;
+   }
+     $message.="
+
    </p>
 
                 <p style='color:#000;font-family:Roboto;font-size:20px'>In case you have any queries regarding your package, please call us on +912261312222 or leave us a mail on info@selfcareindia.com
@@ -188,11 +208,9 @@ print_r($mysession);
 
                 </div></div></div></body></html>";
 
-
-
                 $this->email->message($message);
                 $this->email->send();
-echo $this->email->print_debugger();
+                //echo $this->email->print_debugger();
 
 
          $userquery = $this->db->query("UPDATE `user` SET `firstname`='$firstname',`lastname`='$lastname',`phone`='$phone',`status`='2',`billingline1`='$billingline1',`billingline2`='$billingline2',`billingline3`='$billingline3',`billingcity`='$billingcity',`billingstate`='$billingstate',`billingcountry`='$billingcountry',`billingpincode`='$billingpincode',`shippingline1`='$shippingline1',`shippingline2`='$shippingline2',`shippingline3`='$shippingline3',`shippingcity`='$shippingcity',`shippingcountry`='$shippingcountry',`shippingstate`='$shippingstate',`shippingpincode`='$shippingpincode' WHERE `id`='$user'");
@@ -206,187 +224,4 @@ echo $this->email->print_debugger();
      }
 
 
-  //   public function placeOrder($user,$firstname,$shippingaddress,$shippingpincode,$shippingcity,
-  //   $shippingstate,$shippingcontact,$email, $carts, $paymentmode)
-  //   {
-  //       $mysession = $this->session->all_userdata();
-  //
-  //
-  //           $query = $this->db->query("INSERT INTO `fynx_order`(`user`, `firstname`,`shippingaddress`,`shippingpincode`,`shippingcity`,`shippingstate`,`shippingcontact`,`email`,`orderstatus`) VALUES ('$user','$firstname','$shippingaddress','$shippingpincode','$shippingcity','$shippingstate','$shippingcontact','$email','1')");
-  //
-  //
-  //       $order = $this->db->insert_id();
-  //       $mysession['orderid'] = $order;
-  //       $this->session->set_userdata($mysession);
-  //
-  //      $cartcount=count($carts);
-  //     //  echo "    cart count    ".$cartcount."      "."end";
-  //       foreach ($carts as $cart) {
-  //           $querycart = $this->db->query("INSERT INTO `fynx_orderitem`(`order`, `product`, `quantity`, `price`, `finalprice`,`design`) VALUES ('$order','".$cart['id']."','".$cart['qty']."','".$cart['price']."','".$cart['subtotal']."','".$cart['design']."')");
-  //           $quantity = intval($cart['qty']);
-  //           $productid = $cart['id'];
-  //           $this->db->query("UPDATE `fynx_product` SET `fynx_product`.`quantity`=`fynx_product`.`quantity`-$quantity WHERE `fynx_product`.`id`='$productid'");
-  //       }
-	// $table =$this->order_model->getorderitem($order);
-	// 	$before=$this->order_model->beforeedit($order);
-  //
-  //        $userquery = $this->db->query("UPDATE `user` SET `firstname`='$firstname',`lastname`='$lastname',`phone`='$phone',`status`='2',`billingline1`='$billingline1',`billingline2`='$billingline2',`billingline3`='$billingline3',`billingcity`='$billingcity',`billingstate`='$billingstate',`billingcountry`='$billingcountry',`billingpincode`='$billingpincode',`shippingline1`='$shippingline1',`shippingline2`='$shippingline2',`shippingline3`='$shippingline3',`shippingcity`='$shippingcity',`shippingcountry`='$shippingcountry',`shippingstate`='$shippingstate',`shippingpincode`='$shippingpincode' WHERE `id`='$user'");
-  //      if ($query) {
-  //           return $order;
-  //      } else {
-  //           return false;
-  //        }
-  //   }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-//    function placeorder($user, $firstname, $lastname, $email,$billingcontact,$billingaddress, $billingcity, $billingstate, $billingcountry, $shippingaddress, $shippingcity, $shippingcountry, $shippingstate, $shippingpincode, $billingpincode, $status, $company, $carts, $finalamount, $shippingmethod, $shippingname, $shippingcontact, $customernote)
-//	{
-//
-//        $mysession=$this->session->all_userdata();
-//
-//          if($shippingaddress==""){
-//         $query=$this->db->query("INSERT INTO `order`(`user`, `firstname`, `lastname`, `email`, `billingaddress`, `billingcity`, `billingstate`, `billingcountry`, `shippingaddress`, `shippingcity`, `shippingcountry`, `shippingstate`, `shippingpincode`, `finalamount`, `billingpincode`,`shippingmethod`,`orderstatus`,`customernote`,`billingcontact`,`shippingcontact`) VALUES ('$user','$firstname','$lastname','$email','$billingaddress','$billingcity','$billingstate','$billingcountry','$billingaddress','$billingcity','$billingcountry','$billingstate','$billingpincode','$finalamount','$billingpincode','$shippingmethod','1','$customernote','$billingcontact','$billingcontact')");
-//        }
-//        else{
-//        $query=$this->db->query("INSERT INTO `order`(`user`, `firstname`, `lastname`, `email`, `billingaddress`, `billingcity`, `billingstate`, `billingcountry`, `shippingaddress`, `shippingcity`, `shippingcountry`, `shippingstate`, `shippingpincode`, `finalamount`, `billingpincode`,`shippingmethod`,`orderstatus`,`shippingname`,`shippingcontact`,`customernote`,`billingcontact`) VALUES ('$user','$firstname','$lastname','$email','$billingaddress','$billingcity','$billingstate','$billingcountry','$shippingaddress','$shippingcity','$shippingcountry','$shippingstate','$shippingpincode','$finalamount','$billingpincode','$shippingmethod','1','$shippingname','$shippingcontact','$customernote','$billingcontact')");
-//        }
-//
-//
-//
-//        $billingaddressforuser=$billingaddress;
-//        $shippingaddressforuser=$shippingaddress;
-//
-//        $order=$this->db->insert_id();
-//        $mysession["orderid"]=$order;
-//        $this->session->set_userdata($mysession);
-//        foreach($carts as $cart)
-//        {
-//            $querycart=$this->db->query("INSERT INTO `orderitems`(`order`, `product`, `quantity`, `price`, `finalprice`) VALUES ('$order','".$cart['id']."','".$cart['qty']."','".$cart['price']."','".$cart['subtotal']."')");
-//            $quantity=intval($cart['qty']);
-//            $productid=$cart['id'];
-//            $this->db->query("UPDATE `product` SET `product`.`quantity`=`product`.`quantity`-$quantity WHERE `product`.`id`='$productid'");
-//
-//
-//        }
-//
-//
-//		$table =$this->order_model->getorderitem($order);
-//		$before=$this->order_model->beforeedit($order);
-//
-//        $todaydata=date("Y-m-d");
-//        $this->load->library('email');
-//        $this->email->from('info@magicmirror.in', 'Magicmirror');
-//        $this->email->to($email);
-//        $this->email->subject('Magic Mirror Order');
-//        if($before['order']->billingaddress=="")
-//                        {
-//            $billingaddress=$before['order']->firstname." ".$before['order']->lastname."<br>".$before['order']->shippingaddress."<br>".$before['order']->shippingcity."<br>".$before['order']->shippingstate."<br>".$before['order']->shippingpincode;
-//
-//                        }
-//                        else
-//                        {
-//                            $billingaddress=$before['order']->firstname." ".$before['order']->lastname."<br>".$before['order']->billingaddress."<br>".$before['order']->billingcity."<br>".$before['order']->billingstate."<br>".$before['order']->billingpincode;
-//                        }
-//        if($before['order']->shippingaddress=="")
-//                        {
-//                             $shippingaddress=$before['order']->firstname." ".$before['order']->lastname."<br>".$before['order']->billingaddress."<br>".$before['order']->billingcity."<br>".$before['order']->billingstate."<br>".$before['order']->billingpincode;
-//                        }
-//                        else
-//                        {
-//                             $shippingaddress=$before['order']->firstname." ".$before['order']->lastname."<br>".$before['order']->shippingaddress."<br>".$before['order']->shippingcity."<br>".$before['order']->shippingstate."<br>".$before['order']->shippingpincode;
-//                        }
-//
-//        $message="<html><body style=\"background:url('http://magicmirror.in/emaildata/emailer.jpg')no-repeat center; background-size:cover;\">
-//    <div style='text-align:center; padding-top: 40px;'>
-//        <img src='http://magicmirror.in/emaildata/email.png'>
-//    </div>
-//    <div style='text-align:center;   width: 50%; margin: 0 auto;'>
-//        <h2 style='padding-bottom: 5px;color: #e82a96;'>Orders Details</h2>
-//        <table align='center' border='1' cellpadding='2' cellspacing='0' width='100%' style='border: 0px solid black;padding-bottom: 40px;'>
-//            <tr align='right' style='border: 0px;'>
-//                <td width='70%' style='border: 0px;'>
-//&nbsp;
-//                </td>
-//                     <td width='30%' style='border: 0px;'>
-//                   Date :<span>$todaydata</span>
-//                </td>
-//                                                   </tr>
-//                                                   <tr align='right' style='border: 0px;'>
-//                                                  <td width='70%' style='border: 0px;'>
-//&nbsp;
-//                </td>
-//                                <td width='30%' style='border: 0px;'>
-//                  Invoice No.:<span>$order</span>
-//                </td>
-//            </tr>
-//        </table>
-//
-//        <table align='center' border='1' cellpadding='0' cellspacing='0' width='100%' style='border: 0px solid black;padding-bottom: 40px;'>
-//           <tr>
-//    <th style='padding: 10px 0;'>Billing Address</th>
-//    <th style='padding: 10px 0;'>Shipping Address</th>
-//  </tr>
-//          <tr >
-//              <td width='50%' style='padding: 10px 15px;'>
-//<p>$billingaddress</p>
-//</td>
-//              <td width='50%' style='padding: 10px 15px;'>
-//<p>$shippingaddress</p>
-// </td>
-//  </tr>
-//        </table>
-//
-//                 <table align='center' border='1' cellpadding='0' cellspacing='0' width='100%' style='border: 0px solid black;padding-bottom: 40px;'>
-//  <tr>
-//    <th style='padding: 10px 0;'>Id</th>
-//    <th style='padding: 10px 0;'>Product</th>
-//    <th style='padding: 10px 0;'>Quantity</th>
-//    <th style='padding: 10px 0;'>Price</th>
-//    <th style='padding: 10px 0;'>Total Amount</th>
-//  </tr>";
-//        $count=1;
-//        $finalpricetotal=0;
-//        foreach($table as $row)
-//        {
-//            $namesku=$row->name."-".$row->sku;
-//            $quantity=$row->quantity;
-//            $price=$row->price;
-//            $finalprice=$row->finalprice;
-//            $message.="
-//            <tr>
-//                <td align='center' style='padding: 10px 0;'>$count</td>
-//                <td align='center' style='padding: 10px 0;'>$namesku</td>
-//                <td align='center' style='padding: 10px 0;'>$quantity</td>
-//                <td align='center' style='padding: 10px 0;'>$price</td>
-//                <td align='center' style='padding: 10px 0;'>$finalprice</td>
-//              </tr>";
-//            $finalpricetotal=$finalpricetotal+$value->finalprice;
-//                            $counter++;
-//        }
-//  $message.="
-//
-//        </table>
-//    </div>
-//    <div style='text-align:center;position: relative;'>
-//        <p style=' position: absolute; top: 8%;left: 50%; transform: translatex(-50%); font-size: 1em;margin: 0; letter-spacing:2px; font-weight: bold;'>
-//            Your Order is Pending.
-//        </p>
-//        <img src='http://magicmirror.in/emaildata/magicfooter.png'>
-//    </div>
-//</body>
-//
-//</html>";
-//        $this->email->message($message);
-//        // $this->email->html('<b>hello</b>');
-//        $this->email->send();
-//
-//        $userquery=$this->db->query("UPDATE `user` SET `firstname`='$firstname',`lastname`='$lastname',`phone`='$billingcontact',`status`='$status',`billingaddress`='$billingaddressforuser',`billingcity`='$billingcity',`billingstate`='$billingstate',`billingcountry`='$billingcountry',`shippingaddress`='$shippingaddressforuser',`shippingcity`='$shippingcity',`shippingcountry`='$shippingcountry',`shippingstate`='$shippingstate',`shippingpincode`='$shippingpincode',`companyname`='$company' WHERE `id`='$user'");
-//        if($query){
-//        return $order;
-//        }
-//        else{
-//		return false;
-//        }
-//	}
 }
