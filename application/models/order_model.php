545d6b6160f8e9fee1417ed8801338eb768f2c44
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-03-01 11:19:00 +0530

    chnages

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -109,7 +109,7 @@ class order_model extends CI_Model
 
 // echo"cart";
 // print_r($mysession);
-  print_r($carts);
+  // print_r($carts);
    //        $cartcount=count($carts);
    //        echo "    cart count    ".$cartcount."      "."end";
               foreach ($carts as $cart) {
@@ -121,6 +121,97 @@ class order_model extends CI_Model
              $this->db->query("UPDATE `fynx_product` SET `fynx_product`.`quantity`=`fynx_product`.`quantity`-$quantity WHERE `fynx_product`.`id`='$productid'");
          }
 
+
+         //email to customer
+         $this->load->library('email');
+         $this->email->from('vigwohlig@gmail.com', 'MyFynx');
+         $this->email->to($email);
+         $this->email->subject('Thank You for shopping with us');
+         $message= "test text";
+         $message = "<html><body style='margin: 0;'>
+           <div class='fynxmailer' style='width: 600px; max-width:600px; margin: 0 auto;'>
+             <header>
+               <img src='http://wohlig.co.in/myfynx/img/emailer-fynx.png' alt='' class='img-responsive'>
+             </header>
+             <main>
+               <div class=''>
+                 <div class='section-login' style='margin: 0 20px;'>
+                   <p style='font-family: Roboto;font-size: 20px;color: #000;'>Dear <span style='font-family: Roboto;font-size: 20px;color: #000;'>Manan</span>,</p>
+                   <p style='font-family: Roboto;font-size: 20px;color: #000;'>Thank You for signing up on My Fynx. We are really excited to have you with us!</p>";
+
+         $count=1;
+         $finalpricetotal=0;
+         foreach($carts as $cart)
+         {
+         $id=$cart['id'];
+         $color=$cart['color'];
+         $size=$cart['size'];
+         $quantity=$cart['qty'];
+         $price=$cart['price'];
+         $subtotal=$cart['subtotal'];
+
+         $message.="
+         <table style='width: 100%;'>
+           <thead style=' background-color: #fc483f; color: #fff;'>
+             <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>Item</th>
+             <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>Color</th>
+             <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>Size</th>
+             <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>Quantity</th>
+             <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>SubTotal</th>
+           </thead>
+           <tbody>
+             <tr>
+               <td style='text-align:center;'>
+                 <img src='http://www.myfynx.com/newfynx/uploads/WS05MAIN1.JPG' alt='' width='70' class='img-responsive'>
+               </td>
+               <td style='text-align:center;'>White</td>
+                 <td style='text-align:center;'>S</td>
+                 <td style='text-align:center;'>$quantity</td>
+                 <td style='text-align:center;'>$subtotal</td>
+             </tr>
+           </tbody>
+         </table>
+         ";
+         $finalpricetotal=$finalpricetotal+$value->finalprice;
+                $counter++;
+         }
+         $message.="
+         <p style='font-family: Roboto;font-size: 20px;color: #000;'>Your My Fynx Registered Email Id is : <a href='' style='color: #000;font-size: 20px;text-decoration: none;font-family: Roboto;'>manan@ting.in</a></p>
+         <p style='font-family: Roboto;font-size: 20px;color: #000;'><a href='http://www.myfynx.com' target='_blank' style='font-size: 20px;color: #fc483f;font-family: Roboto;'>Click Here</a> to return to the website.</p>
+         <p style='color: #fc483f;font-family: Roboto;font-size: 20px;'>Happy Shopping !</p>
+         <span style='font-family: Roboto;font-size: 20px;color: #000;'>Thank You,</span>
+         <span class='block' style='font-family: Roboto;font-size: 20px;color: #000;display: block;'>Team My Fynx!</span>
+         </div>
+         </div>
+         </main>
+         <footer style='background-color: #000; padding: 10px 0; color: #fff; margin-top: 20px;'>
+         <div class='footer-wrapper'>
+         <table style='width: 100%;'>
+         <tr>
+           <td style='padding:15px; float:left;'>
+             <div class='copy'>
+               <span style='font-family: Roboto;font-size: 14px;color: #fff;'>COPYRIGHT@MYFYNX2016</span>
+             </div>
+           </td>
+           <td style='padding: 0 15px; text-align: right;'>
+             <div class='follow' style='text-align: center; float: right;'>
+               <span class='block' style='font-family: Roboto;font-size: 14px;color: #fff;display: block;'>FOLLOW US ON</span>
+               <a href='https://www.facebook.com/MyFynx-401315743385366/?fref=ts' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-fb.png' alt='Facebook' width='20'></a>
+               <a href='https://twitter.com/MyFynx' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-twi.png' alt='Twitter' width='20'></a>
+               <a href='https://www.instagram.com/myfynx/' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-insta.png' alt='Instagram' width='20'></a>
+               <a href='https://www.youtube.com/channel/UCIo8qm3zCU8JmDZ1UaHhf3Q' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-youtube.png' alt='Youtube' width='20'></a>
+             </div>
+           </td>
+         </tr>
+         </table>
+         </div>
+         </footer>
+         </div>
+         </body></html>";
+
+         $this->email->message($message);
+         $this->email->send();
+         //echo $this->email->print_debugger();
    //		$table =$this->order_model->getorderitem($order);
    //		$before=$this->order_model->beforeedit($order);
 
@@ -159,7 +250,7 @@ $message= "test text";
            <td align='center' style='padding: 10px 0;'>$id</td>
            <td align='center' style='padding: 10px 0;'>$quantity</td>
            <td align='center' style='padding: 10px 0;'>$price</td>
-        
+
          </tr>
          </table>";
        $finalpricetotal=$finalpricetotal+$value->finalprice;
