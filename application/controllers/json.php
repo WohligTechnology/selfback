532181c9f384e922660af024797b3c8c6b8c53d8
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-03-23 18:08:54 +0530

    emailchanges

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -2558,10 +2558,10 @@ public function getsinglesize()
             $hashvalue = base64_encode($userid.'&access');
             $link = "<a href='http://selfcareindia.com/#/forgotpassword/$hashvalue'>Click here </a> To Reset Your Password.";
 
-            $this->load->library('email');
-            $this->email->from('vigwohlig@gmail.com', 'Selfcare');
-            $this->email->to($email);
-            $this->email->subject('Access Password Changed');
+
+            // $this->email->from('vigwohlig@gmail.com', 'Selfcare');
+            // $this->email->to($email);
+            // $this->email->subject('Access Password Changed');
 
             $message = "<html><body><div id=':1fn' class='a3s adM' style='overflow: hidden;'><div class='HOEnZb'><div class='adm'><div id='q_152da6db6beee01c_0' class='ajR h4' data-tooltip='Hide expanded content' aria-label='Hide expanded content'><div class='ajT'></div></div></div><div class='im'><u></u>
     						 <div style='margin:0'>
@@ -2611,8 +2611,49 @@ public function getsinglesize()
 
 
     						 </div></div></div></body></html>";
-            $this->email->message($message);
-            $this->email->send();
+                 $url = 'https://api.sendgrid.com/';
+                 $user = 'vinodwohlig';
+                 $pass = 'wohlig123';
+                $json_string = array(
+
+                  'to' => array(
+                    $email
+                  ),
+                  'category' => 'test_category'
+                );
+
+                $params = array(
+                   'api_user'  => $user,
+                   'api_key'   => $pass,
+                   'x-smtpapi' => json_encode($json_string),
+                   'to'        => $email,
+                   'subject'   => 'Access password Changed',
+                   'html'      => $message,
+                   'text'      => 'Selfcare',
+                   'from'      => 'info@selfcareindia.com',
+                 );
+
+             		$request =  $url.'api/mail.send.json';
+
+             		// Generate curl request
+             		$session = curl_init($request);
+             		// Tell curl to use HTTP POST
+             		curl_setopt ($session, CURLOPT_POST, true);
+             		// Tell curl that this is the body of the POST
+             		curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+             		// Tell curl not to return headers, but do return the response
+             		curl_setopt($session, CURLOPT_HEADER, false);
+             		// Tell PHP not to use SSLv3 (instead opting for TLS)
+             		curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+             		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+             		// obtain response
+             		$response = curl_exec($session);
+             		curl_close($session);
+
+             		// print everything out
+             		print_r($response);
+
 
             $data['message'] = new stdClass();
             $data['message']->value = true;
