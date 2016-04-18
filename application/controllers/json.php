65ab0ccd3fbe97d8359a4cda74aa60b7cbf20a61
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-04-18 17:58:29 +0530

    update currency changes

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -3098,8 +3098,8 @@ print_r($decryptValues);
 
 
 	}
-  echo "currency is".$currency;
-      //  $data['message'] = $this->restapi_model->updateorderstatusafterpayment($order_id, $nb_order_no, $responsecode, $Amount,$currency);
+  // echo "currency is".$currency;
+       $data['message'] = $this->restapi_model->updateorderstatusafterpayment($order_id, $nb_order_no, $responsecode, $Amount,$currency);
     }
 
     public function COD()
